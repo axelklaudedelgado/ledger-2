3a804ea553c83bcfa6bf6186d684717ed24ebc48
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2025-07-22 09:15:01 +0800

    feat: add index file as server

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+require('express-async-errors')
+const app = express()
+
+const { connectToDatabase } = require('./utils/db')
+const middleware = require('./utils/middleware.js')
+const config = require('./utils/config.js')
+
+app.use(express.json())
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+const start = async () => {
+	await connectToDatabase()
+	app.listen(config.development.port, () => {
+		console.log(`Server running on port ${config.development.port}`)
+	})
+}
+
+start()
